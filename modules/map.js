fcd9e2d3c352cb466e6f6b48973aee9d083f99b0
Jania <jania@Janias-iMac.local>
2017-12-05 02:37:53 -0800

    map

---
+++ modules/map.js
@@ -0,0 +1,66 @@
+
+
+
+
+function display(pic){
+        document.getElementById('zone').src = pic;
+        document.getElementById('zone').style.display='block';
+}
+function zone1(){ 
+        var pic = "../img/zone-1.png"
+        display(pic);
+        }
+
+function zone2(){ 
+        var pic = "../img/zone-2.png"
+        display(pic);
+        }
+
+function zone3(){ 
+        var pic = "../img/zone-3.png"
+        display(pic);
+        }
+
+function zone4(){ 
+        var pic = "../img/zone-4.png"
+        display(pic);
+        }
+
+
+
+//document.querySelector('.zone').addEventListener('click', function(){
+//
+//
+//    var pic = 
+//     
+////    var zone = document.querySelector('.zone');
+////       // changing the imege
+////        zone.src = 'zone-' + pic + '.png';
+//    
+//        document.getElementById('zone').src = pic;
+//        document.getElementById('zone').style.display='block';
+//
+//});
+
+//to make a document.querySelector for the button and to paset in the function 
+
+//
+//function zoone(img) {
+//    
+////    var p = document.getElementById('.id')
+//    
+//        var z1 = "../img/zone-1.png"
+//        var z2 = "../img/zone-2.png"
+//        var z3 = "../img/zone-3.png"
+//        var z4 = "../img/zone-4.png"
+//        
+//    if (img == z1)
+//            display(z1);
+//    if (img == z2)
+//            display(z2);
+//    if (img.z3)
+//            display(z3);
+//    if (img.zone4)
+//            display(z4);
+//    
+//}

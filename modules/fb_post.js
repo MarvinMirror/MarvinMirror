5a9aaed83576f3b1cb0a2f9456e8f0cd681b3be3
Anastasiia Dosiak <adosiak@Anastasiias-MacBook-Pro.local>
2018-02-23 18:07:18 -0800

    fixed slack, calendar, added comments to the rest

--- modules/fb_post.js
+++ modules/fb_post.js
@@ -9,19 +9,25 @@ var Post = require("../src/mongoDB").Models.Post;
 //curl used to test:
 //curl 'https://graph.facebook.com/v2.11/42SiliconValley?fields=posts&access_token=196637397563632|6e5049a9b266fff6438ff0b9d1bf5ff7'
 
+//saving the latest fb_post into database
 var updateFBDB = (data) => {
 	let endpoint = "message" in data.posts.data[0] ? "message" : "story";
 
+	let dots = "";
+  if (data.posts.data[0][endpoint].length > 200)
+    dots = "...";
+
+	// create variables with data you'll store in db
 	let find_query = { "type": "FB" };
 	let update = {
 		"type" : "FB",
 		"timestamp" : moment(data.posts.data[0].created_time).format("ddd, MMMM D, HH:mm"),
-		"message" : data.posts.data[0][endpoint]
+		'message' : data.posts.data[0][endpoint].substring(0, 200) + dots
 	};
 	let options = { upsert: true, new: true };
 
 	var updateFB = Post.findOneAndUpdate(find_query, update, options);
-  
+
 	updateFB.exec( (err) => {
 		if (err) throw err;
 		console.log("updated FB in DB!");
@@ -44,20 +50,19 @@ function fb_post() {
 			if ( "message" || "story" in data.posts.data[0]){
 				updateFBDB(data);
 			}
+
+			//retrieving saved post from database
 			let newData = Post.findOne({ "type": "FB" });
 			newData.exec( (err, x) => {
 				var fb_img = document.createElement("div");
 				var fb_text = document.createElement("div");
-				//var fb_text_time = document.createElement("div");
-  
+
 				var fb_icon = document.createElement("img");
 				fb_icon.setAttribute("src", "../img/fb_icon.png");
-  
+
 				fb_img.append(fb_icon);
 				fb_text.setAttribute("class", "post_text");
-				//fb_text_time.setAttribute("class", "post_text_time");
-  
-				//fb_text_time.innerHTML = dt;
+
 				fb_text.innerHTML = x.timestamp + "<br>" + x.message;
 				fbdiv.append(fb_img, fb_text);
 			});

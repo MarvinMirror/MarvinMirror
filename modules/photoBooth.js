e13432ff4afe3b8a6e40b0c67674b1abf1b1fe44
Kyle Murray <Marvin1@Kyles-iMac.local>
2018-03-09 17:16:30 -0800

    fixed css for actual screen size

--- modules/photoBooth.js
+++ modules/photoBooth.js
@@ -1,49 +1,44 @@
-var watermark = require('watermarkjs');
-var manageDOM = require('../src/manageDOM');
-var Raspistill = require('node-raspistill').Raspistill;
+var watermark = require("watermarkjs");
+var manageDOM = require("../src/manageDOM");
+var Raspistill = require("node-raspistill").Raspistill;
 var options = {
-        noPreview: false,
-        outputDir: '/home/pi/Pictures/Photo',
-        encoding: 'jpg',
-        width: 1024,
-        height: 768,
-//      time:undefined,
-//      iso: 200,
-//      shutterspeed: undefined,
-//      contrast: 10,
-//      brightness: 30,
-//      saturation: 10
+	noPreview: false,
+	outputDir: "/home/pi/Pictures/Photo",
+	encoding: "jpg",
+	width: 1024,
+	height: 768
 };
 
 function showIMG(name){
-        manageDOM.buildPopup();
-        var showPhoto = document.createElement("img");
-        	showPhoto.src = '/home/pi/Pictures/Photo/'+ name + '.jpg';
-    var show = document.getElementById('popup');
-	watermark(['/home/pi/Pictures/Photo/'+name+'.jpg', '../img/42_Logo.png'])
-		.image(watermark.image.lowerLeft(0.5))
-		.then(function (img) {
-            		show.appendChild(img);
-		});
+	manageDOM.buildPopup();
+	manageDOM.array2Div(["photobooth"], "popup");
+	let popup = document.getElementById("popup");
+	let photobooth = document.getElementById("photobooth");
+	popup.appendChild(photobooth);
+	var showPhoto = document.createElement("img");
+	photobooth.className = "photobooth center-div";
+	photobooth.appendChild(showPhoto);
+	showPhoto.src = "/home/pi/Pictures/Photo/"+ name + ".jpg";
+	watermark(["/home/pi/Pictures/Photo/" + name + ".jpg", "../img/42_Logo.png"]).image(watermark.image.lowerLeft(0.5));
 }
 
 function photo(){
 
-        options.fileName = Math.floor((Math.random()*999) + 1);
-        //console.log(options);
+	options.fileName = Math.floor((Math.random()*999) + 1);
+	//console.log(options);
 
-
-        var new_photo = new Raspistill(options);
-        new_photo.takePhoto()
-                .then((photo) => {})
-                .catch((error) => {});
-
-        function saund() {
-            var saund = new Audio("../img/Photo_saund.mp3");
-                saund.play();
-        }
-    setTimeout(function(){ saund(); }, 5000);
-setTimeout(function(){showIMG(options.fileName);}, 6000);
+	var new_photo = new Raspistill(options);
+	new_photo.takePhoto()
+		.then(() => {
+			setTimeout( () => {
+				var sound = new Audio("../img/photo_sound.mp3");
+				sound.play();
+			}, 3000);
+			setTimeout( () => {
+				showIMG(options.fileName);
+			}, 4000);
+		})
+		.catch(console.error);
 }
 
 module.exports = photo;

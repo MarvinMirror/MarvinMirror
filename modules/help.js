1aeb5dd47f64f02335b2a6961381b1ac010e5ade
Kyle Murray <Marvin1@Kyles-iMac.local>
2018-03-17 14:14:38 -0700

    Fixed the help function, made cantina fonts bigger, fixed gifs to content div, full documentation for help function

--- modules/help.js
+++ modules/help.js
@@ -1,33 +1,128 @@
 var manageDOM = require("../src/manageDOM");
 
-var helpHTML = "\
-    <div class=\"help-wrapper center-div\">\
-        <div class=\"help__header\">\
-            <h2>Start with \"Hey Marvin,\" then try:</h2>\
-        </div>\
-        <ul>\
-            <li>What is the weather like in Paris?</li>\
-            <li>Show me the forecast for South Lake Tahoe.</li>\
-            <li>What is the cantina serving today (or tomorrow)?</li>\
-            <li>What is the time in Moscow?</li>\
-            <li>What is the word of the day?</li>\
-            <li>Show me 42's event calendar.</li>\
-            <li>Show a student's info.</li>\
-            <li>Show me a student's location in the lab.</li>\
-            <li>Show me a student's best projects.</li>\
-            <li>Who made you?</li>\
-        </ul>\
-    </div>\
-";
-
+/*	This feature randomly picks from Marvin's features and displays 
+	seven intents and example voice commands to the popup div */
 function marvinHelp() {
 
-	//clear content div
 	manageDOM.clearContent("content");
 	
-	// creating new html
-	manageDOM.buildPopup();
-	document.getElementById("popup").innerHTML = helpHTML;
+	/*	Creating the array of DOM elements to be generated with one of
+		our source functions */
+	var elements = [
+		"help-wrapper center-div",
+		"help-header",
+		"help-content0",
+		"help-content1",
+		"help-content2",
+		"help-content3",
+		"help-content4",
+		"help-content5",
+		"help-content6"	
+	];
+    
+	/*  An array of objects that Marvin will currently pick from to
+        display on the help screen.
+        Current count (for rand generator): 0 to 13 */
+	var actions = [
+		{   
+			intent: "weather",
+			example: "Show me the weather for San Francisco."
+		},
+		{   
+			intent: "forecast",
+			example: "What is the forecast for London?"
+		},
+		{   
+			intent: "cantina",
+			example: "What's on the menu for [today/tomorrow]?"
+		},
+		{   
+			intent: "local time",
+			example: "What time is it in Cape Town?"
+		},
+		{   
+			intent: "word of the day",
+			example: "What is the word of the day?"
+		},
+		{   
+			intent: "events",
+			example: "Show me 42's calendar of events"
+		},
+		{   
+			intent: "42 Student Info",
+			example: "Show me info about a student"
+		},
+		{   
+			intent: "42 Student Corrections",
+			example: "Am I late for a correction?"
+		},
+		{   
+			intent: "42 Student Projects",
+			example: "Show me a student's best projects."
+		},
+		{   
+			intent: "42 Student Location",
+			example: "Find a student for me on the map"
+		},
+		{   
+			intent: "Author",
+			example: "Who made you?"
+		},
+		{   
+			intent: "News",
+			example: "What's in the news?"
+		},
+		{   
+			intent: "Wikipedia",
+			example: "Show me the wiki for James Bond."
+		},
+		{   
+			intent: "Photobooth",
+			example: "Snap a picture of me!"
+		}
+	];
+
+	/*	Uses the 'elements' array to create DOM elements to build from */
+	manageDOM.array2Div(elements);
+
+	/*	Fills the text within the header box */
+	document.getElementById("help-header").innerHTML = "How can Marvin help you?";
+
+	/*  To keep track and not double up on instructions */
+	let int_arr = [];
+
+	/*	Loops through seven times to pick seven random intents without doubling up.
+		This function creates a div each for the intent and example and appends those
+		to existing content elements with appropriate text inside */
+	for (let i = 0; i < 7 ; i++){
+		let content = document.getElementById("help-content" + i);
+		content.className = "help-content";
+		
+		let intent = document.createElement("div");
+		intent.className = "help-intent";
+		let example = document.createElement("div");
+		example.className = "help-example";
+		
+		content.append(intent, example);
+        
+		let flag = 0;
+		let int;
+
+		/*	Ensures there will be no duplicates selected */
+		while (flag === 0) {
+			int = Math.floor(Math.random() * 14);
+			if (int_arr.includes(int)) {
+				flag = 0;
+			}
+			else {
+				flag = 1;
+				int_arr.push(int);
+			}
+		}
+
+		intent.innerHTML = actions[int].intent;
+		example.innerHTML = actions[int].example;
+	}
 }
 
 module.exports = marvinHelp;
--- src/controller.js
+++ src/controller.js
@@ -1,88 +1,87 @@
+let manageDOM = require("./manageDOM");
+
 var marvinReacts = {
    
-    listen_gif : (event, message) => {
-        manageDOM.clearContent('content');
-        // if (document.getElementById('popup')) document.body.removeChild(document.getElementById('popup'));
-        manageDOM.delPopup();
-        manageDOM.array2Div(["sound_gif","sound_gif"]);
-        var sound_div = document.getElementById('sound_gif');
-        var gif = document.createElement("img");
-        gif.setAttribute("src", "../img/giphy.gif");
-        sound_div.appendChild(gif);
-    },
+    /********* THIS AND PROCESS ARE ESSENTIALLY THE SAME, CAN MAKE mgDOM FUNCTION ********************/
+	listen_gif : () => {
+		manageDOM.clearContent("content");  
+        
+		let content = document.getElementById("content");
+		let listen_gif = document.createElement("div");
+		listen_gif.className = "listen-gif center-div";
+		let gif = document.createElement("img");
+		content.appendChild(listen_gif);
+		gif.setAttribute("src", "../img/giphy.gif");
+		listen_gif.appendChild(gif);
+	},
 
-   process_gif: () => {
-        manageDOM.clearContent("content");
-        manageDOM.array2Div(["process_gif","process_gif"]);
-        var sound_div = document.getElementById('process_gif');
-        var gif = document.createElement("img");
-        gif.setAttribute("src", "../img/giphy2.gif");
-        sound_div.appendChild(gif);
-    },
+	process_gif: () => {
+		manageDOM.clearContent("content");
+        
+		let content = document.getElementById("content");
+		let process_gif = document.createElement("div");
+		process_gif.className = "process-gif center-div";
+		let gif = document.createElement("img");
+		gif.setAttribute("src", "../img/giphy2.gif");
+		content.appendChild(process_gif);
+		process_gif.appendChild(gif);
+	},
 
-   delete_gif : (event, message) => {
-        manageDOM.clearContent("content");
-    },
+    /******************* DO WE NEED THIS FUNCTION *******************/
+	delete_gif : () => {
+		manageDOM.clearContent("content");
+	},
 
-   marvin_gif: () => {
-        var phrases = [
-        "Start with \"Hey Marvin...\"",
-        "Ask me about the weather",
-        "What is the answer to the Ultimate Question?",
-        "I'm feeling very depressed",
-        "Don't talk to me about life",
-        "Here I am with a brain the size of a planet...",
-        "Hey you, YES YOU"];
-        manageDOM.clearContent("content");
-        manageDOM.array2Div(["marvin","marvin_gif", "spot1", "spot2", "spot3", "spot4", "spot5"]);
-        var marvin_div = document.getElementById('marvin_gif');
-        var gif = document.createElement("img");
-        gif.setAttribute("src", "../img/marvin.gif");
-        marvin_div.appendChild(gif);
-        var timer;
-        timer = random_phrases(0, phrases);
+	marvin_gif: () => {
+		var phrases = [
+			"Start with \"Hey Marvin...\"",
+			"Ask me about the weather",
+			"What is the answer to the Ultimate Question?",
+			"I'm feeling very depressed",
+			"Don't talk to me about life",
+			"Here I am with a brain the size of a planet...",
+			"Hey you, YES YOU"];
+		manageDOM.clearContent("content");
+		manageDOM.array2Div(["marvin","marvin_gif", "spot1", "spot2", "spot3", "spot4", "spot5"]);
+		var marvin_div = document.getElementById("marvin_gif");
+		var gif = document.createElement("img");
+		gif.setAttribute("src", "../img/marvin.gif");
+		marvin_div.appendChild(gif);
+		var timer;
+		timer = random_phrases(0, phrases);
 
-       function random_phrases(spot_one, phrases) {
-            if (spot_one != '0') document.getElementById('spot'+ spot_one).innerHTML = "";
-            console.log("spot one:" + spot_one);
-            var text = phrases[Math.floor(Math.random() * phrases.length)];
-            var spot_two = Math.floor(Math.random() * 5) + 1;
-            document.getElementById('spot'+ spot_two).innerHTML = text;
-            console.log("spot two:" + spot_two);
+		function random_phrases(spot_one, phrases) {
+			if (spot_one != "0") document.getElementById("spot"+ spot_one).innerHTML = "";
+			console.log("spot one:" + spot_one);
+			var text = phrases[Math.floor(Math.random() * phrases.length)];
+			var spot_two = Math.floor(Math.random() * 5) + 1;
+			document.getElementById("spot"+ spot_two).innerHTML = text;
+			console.log("spot two:" + spot_two);
 
-            clearTimeout(timer);
-            timer = setTimeout(random_phrases, 3000, spot_two, phrases)
-        }
-    },
+			clearTimeout(timer);
+			timer = setTimeout(random_phrases, 3000, spot_two, phrases);
+		}
+	},
 
-   empty_response: () => {
-        manageDOM.clearContent("content");
-        console.log(document.getElementById('popup'))
-        // setTimeout(talkToMe_dialog, 1500)
-    },
-}
+	empty_response: () => {
+		manageDOM.clearContent("content");
+		console.log(document.getElementById("popup"));
+		// setTimeout(talkToMe_dialog, 1500)
+	},
+};
 var send_message = (message) => {
-        manageDOM.clearContent("content");
-        manageDOM.delPopup();
-        manageDOM.array2Div(["message"]);
-        var message_div = document.getElementById('message');
-        message_div.className += ' center-div';
-        message_div.innerHTML = message;
-    }
+	manageDOM.clearContent("content");
+	manageDOM.delPopup();
+	manageDOM.array2Div(["message"]);
+	var message_div = document.getElementById("message");
+	message_div.className += " center-div";
+	message_div.innerHTML = message;
+};
 
 
 function talkToMe_dialog() {
-    var dialog = document.getElementById('content');
-    if (!dialog.innerHTML.length && !document.getElementById('popup')) marvinReacts.marvin_gif();
-}
-
-function gif() {
-    manageDOM.clearContent("content");
-    manageDOM.array2Div(["sound_gif","sound_gif"]);
-    var sound_div = document.getElementById('sound_gif');
-    var gif_img = document.createElement("img");
-    gif_img.setAttribute("src", "../img/giphy.gif");
-    sound_div.appendChild(gif_img);
+	var dialog = document.getElementById("content");
+	if (!dialog.innerHTML.length && !document.getElementById("popup")) marvinReacts.marvin_gif();
 }
 
 module.exports = marvinReacts;
